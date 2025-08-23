d44113dd2841d56c27365a33dea97ddafb4ca2b0
Prince-Mann94 <princemann2007@gmail.com>
2025-08-23 16:29:26 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter first no. :");
+    scanf("%d",&a);
+
+    printf("Enter second no. :");
+    scanf("%d",&b);
+    printf("Before swap :%d %d\n", a , b);
+
+    c = a;
+    a = b;
+    b = c;
+    printf("After swap : %d %d\n", a , b);
+
+    return 0;
+}
