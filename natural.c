9f7a675e0a0047a7397a87774605aeb16acc6bdb
Prince-Mann94 <princemann2007@gmail.com>
2025-08-24 10:05:20 +0530

    Update natural.c

--- natural.c
+++ natural.c
@@ -1,3 +1,5 @@
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+
 #include<stdio.h>
 int main(){
     int n;
@@ -5,4 +7,5 @@ int main(){
     scanf("%d",&n);
 
     printf("Sum of first n natural no.s :%d",(n*(n+1)/2));
+
 }
