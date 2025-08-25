b004e5997c3e2f82ad270e7cf7d1825902551614
Prince-Mann94 <princemann2007@gmail.com>
2025-08-25 17:14:23 +0530

    Update SI_CI.c

--- SI_CI.c
+++ SI_CI.c
@@ -15,4 +15,7 @@ int main(){
 
     printf("Simple Interest :%.2f\n",((principal * rate * time)/100.0));
     printf("Compound Interest :%.2f\n",(principal * pow((1 + rate/100.0),time)- principal));
+
+    return 0;
+
 }
