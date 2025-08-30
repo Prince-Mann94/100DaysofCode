d1e358f04a981517cbf9b5e1f102851e4224d53d
Prince-Mann94 <princemann2007@gmail.com>
2025-08-30 09:48:22 +0530

    update add.c

--- Day 1/add.c
+++ Day 1/add.c
@@ -1,1 +1,19 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter first no. :");
+    scanf("%d",&a);
+
+    printf("Enter second no. :");
+    scanf("%d",&b);
+
+    c = a+b;
+    printf("Sum of a and b :%d",c);
+
+    return 0;
+
+}
+
 
