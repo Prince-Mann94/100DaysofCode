58d4208a82523a40de3a9228bda9404f586a645e
Prince-Mann94 <princemann2007@gmail.com>
2025-09-03 10:29:42 +0530

    Create sum_odd.c

---
+++ Day 14/sum_odd.c
@@ -0,0 +1,11 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. :");
+    scanf("%d",&n);
+
+    printf("sum of first 'n' odd no. : %d",n*n);
+return 0;
+}
