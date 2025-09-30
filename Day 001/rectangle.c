890c94be86cf678a8f9905c5fe635275606d4ea3
Prince-Mann94 <princemann2007@gmail.com>
2025-09-30 22:01:24 +0530

    Delete Day 001/rectangle.c

--- Day 001/rectangle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int length , breadth;
-    length = 5;
-    breadth = 10;
-
-    printf("Area of rectangle :%d\n",length * breadth);
-    printf("Perimeter of rectangle :%d\n",2*(length + breadth));
-}
