fda9c9f09af8867166c129077d01569fc76376d3
Prince-Mann94 <princemann2007@gmail.com>
2025-09-30 21:59:45 +0530

    Add files via upload

---
+++ Day 001/rectangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int length , breadth;
+    length = 5;
+    breadth = 10;
+
+    printf("Area of rectangle :%d\n",length * breadth);
+    printf("Perimeter of rectangle :%d\n",2*(length + breadth));
+}
